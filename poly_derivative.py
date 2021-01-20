fc8dc3b07aeeef7088ff54b47a6d642377a7265d
jadelux <renanqi2008@gmail.com>
2021-01-20 19:28:49 +0800

    your commit message

--- poly_derivative.py
+++ poly_derivative.py
@@ -1,3 +1,9 @@
+import math
+def f(x):
+    y = (a0)+(a1*x)+a2*x**2+a3*x**3
+    return y
+
+
 # Polynomial coefficients
 a0 = 1
 a1 = -2
@@ -5,10 +11,9 @@ a2 = 4.6
 a3 = 0.2
 
 # Point at which we evaluate the derivative
-x0 = 3.1
-
-
-p_prime = # ...
+x = 3.1
+p_prime = a1+2*a2*x+3*a3*x**2
+print(p_prime)
 
 
 
